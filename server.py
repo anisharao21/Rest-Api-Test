74b0c02381532e3094faa47208a4f9581f69b86d
Anisha <anisha@staging.unifyvault>
2019-01-17 20:04:48 +0000

    commiting the server file

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from json import dumps
 
 
 
-db_connect = create_engine('sqlite:////home/anisha/python_rest/chinook.db')
+db_connect = create_engine('sqlite:////home/anisha/projects/src/github.com/anisharao21/Rest-Api-Test/chinook.db')
 from pprint import pprint
 #pprint(dir(db_connect.table_names))
 app = Flask(__name__)
@@ -40,3 +40,5 @@ api.add_resource(Employees, '/employees') # Route_1
 api.add_resource(Tracks, '/tracks') # Route_2
 api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
 
+if __name__ == '__main__':
+     app.run(host='0.0.0.0',port='5002')
